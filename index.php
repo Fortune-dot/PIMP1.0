3766237099009dd23f4a80cfa421592c505df0a9
Fortune <fortunelangat54@gmail.com>
2022-06-23 06:20:20 -0700

    better message display

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if(isset($_POST['submit'])){
     
     
     if ($msg_resp ->success == 'true') {
-        echo "WAIT FOR  STK POP UP🔥";
+        echo "<center> <h2>WAIT FOR  STK POP UP🔥</h2></center>";
       } else {
         echo "Transaction Failed";
        
@@ -54,7 +54,7 @@ if(isset($_POST['submit'])){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Untitled</title>
+    <title>PIMP 1.0 🔥😎</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="./style.css">
